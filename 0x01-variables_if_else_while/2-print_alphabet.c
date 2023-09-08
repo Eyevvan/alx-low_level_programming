afceb9a8a73730e3e87d154f2dd67638220ee9fc
Evans Murithi <ivanmurithi4@gmail.com>
2023-09-08 15:51:23 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *  main - this is the function of the task
+ *  Return: o
+ */
+
+	int main(void)
+
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	putchar(i);
+	putchar('\n');
+
+	return (0);
+
+}
